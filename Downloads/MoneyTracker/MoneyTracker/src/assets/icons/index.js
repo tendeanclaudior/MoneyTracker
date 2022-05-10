4dee52d9372f056de4da8973071da8782b68eada
tendeanclaudio <99817675+tendeanclaudior@users.noreply.github.com>
2022-05-10 20:25:40 +0800

    Delete Downloads/MoneyTracker/MoneyTracker directory

--- Downloads/MoneyTracker/MoneyTracker/src/assets/icons/index.js
+++
@@ -1,6 +0,0 @@
-import Logo from './Logo.svg';
-import IconBack from './icon-back.svg';
-import Frame from './Frame.svg';
-import Ic_home from './ic_home.svg';
-
-export {Logo, IconBack, Frame, Ic_home};
--- Downloads/MoneyTracker/MoneyTracker/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import SplashScreen from './SplashScreen';
-import SingIn from './SingIn';
-import SingUp from './SingUp';
-import Home from './Home';
-import CashOnHand from './CashOnHand';
-import CashOnBank from './CashOnBank';
-
-export {SplashScreen, SingIn, SingUp, Home, CashOnHand, CashOnBank};
